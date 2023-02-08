1dff311e41a324ac4ffe3e0b377ee0680d71489e
Tanjila Akhtar <37596431+tanjila-tisha@users.noreply.github.com>
2023-02-08 14:56:05 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CompanyList from "./components/CompanyList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Company Portal</h1>
+      <CompanyList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CompanyList.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const CompanyList = (): JSX.Element => {
+  return (
+    <>
+      <div>Here is company list</div>
+      <select className="px-4 rounded-full">
+        <option>type1</option>
+        <option>type1</option>
+        <option>type1</option>
+      </select>
+    </>
+  );
+};
+
+export default CompanyList;
